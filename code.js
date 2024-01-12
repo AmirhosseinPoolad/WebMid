ac58c4e84ba5388ee2dab31ce88bd0db8ce6f28c
Amirhossein Poolad <amirhossein.poolad@gmail.com>
2024-01-12 22:40:10 +0330

    Add style

--- code.js
+++ code.js
@@ -1,8 +1,12 @@
+//run when submit button pressed
 function onsubmit(event) {
+    //avoid default form submit function
     event.preventDefault()
+    //get data from form
     var form = document.getElementById("submit-form")
     var formData = Object.fromEntries(new FormData(form));
     var query = new URLSearchParams({ name: formData.name })
+    //request from api and change DOM accordingly
     fetch("https://api.genderize.io/?" + query, { method: 'get' })
         .then((resp) => resp.json())
         .then(
@@ -13,32 +17,41 @@ function onsubmit(event) {
         );
 }
 
+//changes the dom
 function changeDOM(name, probability, gender){
     document.getElementById("pred-box").innerHTML = gender + "<br/>" + probability
     document.getElementById("saved-box").innerHTML = getSaved(name).name
 }
 
+//gets gender saved in localStorage
 function getSaved(name) {
     return { name: localStorage.getItem(name) }
 }
 
+//run when save button pressed
 function saveClick(event) {
+    //stop default button handling and propogation to parent
     event.preventDefault()
     event.stopPropagation()
+    //get name and gender from form and save in localStorage
     var form = document.getElementById("submit-form")
     var formData = Object.fromEntries(new FormData(form));
     console.log(formData.gender)
     localStorage.setItem(formData.name, formData.gender);
 }
 
+//run when clear button pressed
 function clearClick(event) {
+    //stop default button handling and propogation to parent
     event.preventDefault()
     event.stopPropagation()
+    //get name from form and remove from localStorage
     var form = document.getElementById("submit-form")
     var formData = Object.fromEntries(new FormData(form));
     localStorage.removeItem(formData.name)
 }
 
+//bind functions to buttons
 document.getElementById("submit-form")
     .addEventListener("submit", onsubmit);
 
